3c222f5575bb1ecb4894e7c7dc3413af92fd110a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-11-11 03:36:30 +0900

    feat: Update Username

--- src/main/kotlin/me/bossm0n5t3r/jmolecules/domains/Username.kt
+++ src/main/kotlin/me/bossm0n5t3r/jmolecules/domains/Username.kt
@@ -1,11 +1,12 @@
 package me.bossm0n5t3r.jmolecules.domains
 
-import org.jmolecules.ddd.annotation.ValueObject
+import org.jmolecules.ddd.types.Identifier
+import org.jmolecules.ddd.types.ValueObject
 
-@ValueObject
 data class Username(
     val value: String,
-) {
+) : ValueObject,
+    Identifier {
     init {
         require(value.isNotBlank()) { "Username value cannot be null or blank." }
     }
