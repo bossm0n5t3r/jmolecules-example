753bee298472a65be087cf01f3c8d33baa38dd4a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-11-11 03:33:40 +0900

    feat: Update Slug

--- src/main/kotlin/me/bossm0n5t3r/jmolecules/domains/Slug.kt
+++ src/main/kotlin/me/bossm0n5t3r/jmolecules/domains/Slug.kt
@@ -1,14 +1,17 @@
 package me.bossm0n5t3r.jmolecules.domains
 
-import org.jmolecules.ddd.annotation.ValueObject
+import org.jmolecules.ddd.types.Identifier
+import org.jmolecules.ddd.types.ValueObject
 
-@ValueObject
 data class Slug(
     val value: String,
-) {
+) : ValueObject,
+    Identifier {
     init {
         require(value.isNotBlank()) { "Slug must not be blank" }
         require(value.length >= 5) { "Article's slug should be at least 5 characters long!" }
         require(value.length <= 100) { "Article's slug should be at most 100 characters long!" }
     }
 }
+
+fun String.toSlug(): Slug = Slug(this.lowercase().replace(" ", "-"))
