d7a9c1f12c71e71f86add9a6d703527503655a18
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-11-14 04:31:30 +0900

    feat: Add JMoleculesExampleApplication

---
+++ src/main/kotlin/me/bossm0n5t3r/jmolecules/JMoleculesExampleApplication.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.jmolecules
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JMoleculesExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<JMoleculesExampleApplication>(*args)
+}
